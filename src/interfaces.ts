9afda458c5d76228b86901c46749cba04a444e04
Robert Costley <ncostley@udel.edu>
2022-04-07 14:48:50 -0400

    Added interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+export {};
+export interface course {
+    name: string;
+    prereqs: string;
+    credits: number;
+    taken: boolean;
+}
+
+export interface degree {
+    taken: boolean;
+    type: string;
+}
+
+export interface semester {
+    session: string;
+    year: number;
+}
