341a7b4ccc1825678f8d2307c89598653c717cb7
rohanyar <rohan3yarlagadda@gmail.com>
2022-04-14 14:34:16 -0400

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,12 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript<br></br>
-                Nick Costley<br></br>
-                Marvin Dang<br></br>
-                Rohan Yarlagadda<br></br>
+                UD CIS Scheduler<br></br>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Nick Costley, Marvin Dang, Rohan
+                Yarlagadda
             </p>
             {/*<SessionPicker></SessionPicker>;*/}
         </div>
