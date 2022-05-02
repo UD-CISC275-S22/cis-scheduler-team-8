d5d144244864f28df6fe165ae58608fd46fd1128
rohanyar <rohan3yarlagadda@gmail.com>
2022-05-02 10:25:46 -0400

    updated code

--- src/session-picker.tsx
+++ src/session-picker.tsx
@@ -3,7 +3,8 @@ import { Form } from "react-bootstrap";
 import { session } from "./interfaces";
 
 export function SessionPicker(): JSX.Element {
-    const [session, setSession] = useState<string>("Semester");
+    const [edit, setEdit] = useState<boolean>(false);
+    const [session, setSession] = useState<string>("Fall");
     const [year, setYear] = useState<string>("2022");
     type ChangeEvent = React.ChangeEvent<
         HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
@@ -16,20 +17,34 @@ export function SessionPicker(): JSX.Element {
     }
     return (
         <div>
-            {" "}
-            <Form.Group controlId="sessions">
-                <Form.Label>Session Season</Form.Label>
-                <Form.Select value={session} onChange={updateSemester}>
-                    <option value="Fall">Fall</option>
-                    <option value="Winter">Winter</option>
-                    <option value="Spring">Spring</option>
-                    <option value="Summer">Summer</option>
-                </Form.Select>
-            </Form.Group>
-            <Form.Label>Session Year</Form.Label>
-            <Form.Control value={year} onChange={updateYear} />
+            {edit && (
+                <Form.Group controlId="sessions">
+                    <Form.Label>Session Season</Form.Label>
+                    <Form.Select value={session} onChange={updateSemester}>
+                        <option value="Fall">Fall</option>
+                        <option value="Winter">Winter</option>
+                        <option value="Spring">Spring</option>
+                        <option value="Summer">Summer</option>
+                    </Form.Select>
+                </Form.Group>
+            )}
+            {edit && (
+                <Form.Group controlId="year">
+                    <Form.Label>Session Year</Form.Label>
+                    <Form.Control value={year} onChange={updateYear} />
+                </Form.Group>
+            )}
             <br></br>
             {session} {year}
+            <div>
+                <Form.Check
+                    type="switch"
+                    id="is-switch-on"
+                    label="Edit"
+                    checked={edit}
+                    onChange={() => setEdit(!edit)}
+                />
+            </div>
         </div>
     );
 }
