0627589c3e43ddfc677c994d64d5502c2f2526af
rohanyar <rohan3yarlagadda@gmail.com>
2022-05-02 10:23:24 -0400

    button attempt

--- src/degree-plan.tsx
+++ src/degree-plan.tsx
@@ -1,17 +1,55 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import { SessionPicker } from "./session-picker";
 
 export function DegreePlan(): JSX.Element {
-    const [name, setName] = useState<string>("test");
-    function degreeName(event: React.ChangeEvent<HTMLInputElement>) {
-        setName(event.target.value);
+    const [name, setName] = useState<string>("Plan");
+    const [visible, setVisible] = useState<boolean>(false);
+    const [semVisible, setSemVisible] = useState<boolean>(false);
+    if (visible && semVisible) {
+        return (
+            <div>
+                <Button onClick={() => setVisible(true)}>Add Plan</Button>
+                <Form.Group controlId="degreeName">
+                    <Form.Label>Plan Name</Form.Label>
+                    <Form.Control
+                        value={name}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setName(event.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Button onClick={() => setSemVisible(true)}>
+                    Add Semester
+                </Button>
+                <SessionPicker></SessionPicker>
+                {/* <Button onClick={() =>}>Save</Button> */}
+            </div>
+        );
+    } else if (visible) {
+        return (
+            <div>
+                <Button onClick={() => setVisible(true)}>Add Plan</Button>
+                <Form.Group controlId="degreeName">
+                    <Form.Label>Plan Name</Form.Label>
+                    <Form.Control
+                        value={name}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setName(event.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Button onClick={() => setSemVisible(true)}>
+                    Add Semester
+                </Button>
+                {/* <Button onClick={() =>}>Save</Button> */}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <Button onClick={() => setVisible(true)}>Add Plan</Button>
+            </div>
+        );
     }
-    return (
-        <div>
-            <Form.Group controlId="degreeName">
-                <Form.Label>Plan Name</Form.Label>
-                <Form.Control value={name} onChange={degreeName}></Form.Control>
-            </Form.Group>
-        </div>
-    );
 }
