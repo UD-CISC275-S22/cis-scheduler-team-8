2a5eb3cbc525829dc1e9d9e9b6031e4651d67f3b
rohanyar <rohan3yarlagadda@gmail.com>
2022-05-02 23:23:16 -0400

    update with dropdown finished

--- src/degree-plan.tsx
+++ src/degree-plan.tsx
@@ -5,71 +5,83 @@ import { degree } from "./Interfaces/plan";
 import { semester } from "./Interfaces/semester";
 import { course } from "./Interfaces/course";
 
-interface PassNewDegree {
-    addDegree: (n: number, r: string, s: semester[]) => void;
-}
+const defaultCourses: course[] = [
+    {
+        code: "CISC108",
+        name: "Introduction to Computer Science I",
+        prereqs: "None",
+        credits: 4,
+        taken: true
+    }
+];
+const defaultSem: semester[] = [
+    {
+        id: 1,
+        session: "Fall",
+        year: "2022",
+        courses: defaultCourses
+    }
+];
+const degreeList: degree[] = [
+    {
+        id: 1,
+        name: "Default Plan",
+        semester: defaultSem
+    }
+];
 
 export function DegreePlan(): JSX.Element {
-    // ask how to use interfaces!
     const [name, setName] = useState<string>("Plan");
     const [session, setSession] = useState<string>("Fall");
-    const defaultCourses: course[] = [
-        {
-            code: "CISC108",
-            name: "Introduction to Computer Science I",
-            prereqs: "None",
-            credits: 4,
-            taken: true
-        }
-    ];
-    const defaultSem: semester[] = [
-        {
-            id: 1,
-            session: "Fall",
-            year: "2022",
-            courses: defaultCourses
-        }
-    ];
-    const defaultPlan: degree[] = [
-        {
-            id: 1,
-            name: "Default Plan",
-            semester: defaultSem
-        }
-    ];
-    type ChangeEvent = React.ChangeEvent<
-        HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
-    >;
-    const plans = ["Degree Plan"];
-    function updatePlans(Dplans: string[], event: ChangeEvent) {
-        const [plan, setPlan] = useState<degree[]>(defaultPlan);
-        const newPlanList = [...Dplans, event.target.value];
-        function addDegree(
-            newId: number,
-            newName: string,
-            newSemester: semester[]
-        ) {
-            const newPlan = [
-                ...plan,
-                {
-                    id: newId,
-                    name: newName,
-                    semester: newSemester
-                }
-            ];
-            setPlan(newPlan);
-        }
+    const [plans, setPlan] = useState<degree[]>(degreeList);
+    function updateSemester(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSession(event.target.value);
+    }
+    function updatePlans() {
+        const newPlanList = [
+            ...plans,
+            {
+                id: 0, //TODO figure out id updates
+                name: name,
+                semester: []
+            }
+        ];
+        setPlan(newPlanList);
     }
     return (
         <div>
-            <Button onClick={() => updatePlans}> Add Plan </Button>
-            <Form.Group controlId="degreeName">
-                <Form.Label>Plan Name</Form.Label>
-                <Form.Control
-                    value={name}
-                    onChange={updatePlans}
-                ></Form.Control>
-            </Form.Group>
+            <div>
+                <Form.Group controlId="sessions">
+                    <Form.Label>Plans</Form.Label>
+                    <Form.Select value={session} onChange={updateSemester}>
+                        {plans.map((item) => {
+                            return (
+                                <option key={item.name} value={item.name}>
+                                    {" "}
+                                    {item.name}{" "}
+                                </option>
+                            );
+                        })}
+                    </Form.Select>
+                </Form.Group>
+            </div>
+            <div>
+                <Button onClick={() => updatePlans()}> Add Plan </Button>
+                <Form.Group controlId="degreeName">
+                    <Form.Label>Plan Name</Form.Label>
+                    <Form.Control
+                        value={name}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setName(event.target.value)}
+                    />
+                </Form.Group>
+            </div>
         </div>
     );
 }
+//TODO LIST
+/*
+1. figure out how to update id's
+2. figure out how to handle new plan creation (creating new plan and jumping to semester buttons etc.)
+*/
