e819c9a3515b67e9bec1eb198ed39950b48cbf23
kinggummmy <44405887+kinggummmy@users.noreply.github.com>
2022-05-11 00:48:39 -0400

    Update poolOfCourses.tsx

--- src/Components/poolOfCourses.tsx
+++ src/Components/poolOfCourses.tsx
@@ -1,16 +1,6 @@
-/*import CISC from "../Data/catalog.json";
-import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import React from "react";
 
 export function Pool(): JSX.Element {
-    return (
-        <div>
-            {CISC.map(
-                (course: course): JSX.Element => (
-                    <p key={course.code}>{course.code}</p>
-                )
-            )}
-            ;
-        </div>
-    );
-}*/
+    return <div></div>;
+}
+
